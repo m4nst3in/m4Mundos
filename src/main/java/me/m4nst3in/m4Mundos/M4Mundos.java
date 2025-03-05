b393f01c348fb92627bff58436eaaf38ab5ca420
M4nsT3iN_ <m4nsdev@unoeste.edu.br>
2025-03-04 22:24:14 -0300

    first commit

---
+++ src/main/java/me/m4nst3in/m4Mundos/M4Mundos.java
@@ -0,0 +1,17 @@
+package me.m4nst3in.m4Mundos;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class M4Mundos extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
